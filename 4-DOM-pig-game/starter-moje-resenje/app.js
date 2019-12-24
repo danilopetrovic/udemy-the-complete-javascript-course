62c1d6e777df2c5f9ab1c3314929fd80d450cc4b
Danilo Petrovic <danilo86petrovic@gmail.com>
2019-12-24 02:55:12 +0100

    04 JavaScript in the Browser DOM Manipulation and Events | lekcije 043 - 050

---
+++ 4-DOM-pig-game/starter-moje-resenje/app.js
@@ -0,0 +1,33 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+*/
+const scores = [0, 0];
+const roundScore = 0;
+const activePlayer = 1;
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function () {
+    // 1. Random number
+    let dice = Math.ceil(Math.random() * 6);
+    // document.querySelector('#current-' + activePlayer).textContent = dice;
+
+    // 2. Display the result
+    let diceDom = document.querySelector('.dice');
+    diceDom.style.display = 'block';
+    diceDom.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round score IF the rolled number is NOT 1
+});
