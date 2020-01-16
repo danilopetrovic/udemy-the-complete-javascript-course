337835338b4f7924889ffcaef78a86f2181e1adc
Danilo Petrovic <danilo86petrovic@gmail.com>
2020-01-16 22:50:14 +0100

    063 Creating Objects | Object.create

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,4 +1,7 @@
+/*
+// Function construktor
 var john = {
+
     name: 'John',
     yearOfBirth: 1990,
     job: 'teacher'
@@ -27,3 +30,23 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+*/
+
+// Object.create
+
+var personProto = {
+    calculateAge: function () {
+        console.log(2016 - this.yearOfBirth)
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto, {
+    name: {value: 'Jane'},
+    yearOfBirth: {value: 1969},
+    job: {value: 'desigher'}
+})
