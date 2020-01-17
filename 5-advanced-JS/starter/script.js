66e116842b3fd0fe1cf453c84cab9e0d38fdfdca
Danilo Petrovic <danilo86petrovic@gmail.com>
2020-01-17 20:53:46 +0100

    065 First Class Functions Passing Functions as Arguments'

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -1,20 +1,20 @@
+/////////////////////////////
+// Lecture: Function constructor
 /*
-// Function construktor
 var john = {
-
     name: 'John',
     yearOfBirth: 1990,
     job: 'teacher'
 };
 
-var Person = function (name, yearOfBirth, job) {
+var Person = function(name, yearOfBirth, job) {
     this.name = name;
     this.yearOfBirth = yearOfBirth;
     this.job = job;
-};
+}
 
-Person.prototype.calculateAge = function () {
-    console.log(2016 - this.yearOfBirth)
+Person.prototype.calculateAge  = function() {
+    console.log(2016 - this.yearOfBirth);
 };
 
 Person.prototype.lastName = 'Smith';
@@ -32,11 +32,14 @@ console.log(jane.lastName);
 console.log(mark.lastName);
 */
 
-// Object.create
 
+
+/////////////////////////////
+// Lecture: Object.create
+/*
 var personProto = {
-    calculateAge: function () {
-        console.log(2016 - this.yearOfBirth)
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
     }
 };
 
@@ -46,7 +49,89 @@ john.yearOfBirth = 1990;
 john.job = 'teacher';
 
 var jane = Object.create(personProto, {
-    name: {value: 'Jane'},
-    yearOfBirth: {value: 1969},
-    job: {value: 'desigher'}
-})
+    name: { value: 'Jane' },
+    yearOfBirth: { value: 1969 },
+    job: { value: 'designer' }
+});
+*/
+
+
+
+/////////////////////////////
+// Lecture: Primitives vs objects
+/*
+// Primitives
+var a = 23;
+var b = a;
+a = 46;
+console.log(a);
+console.log(b);
+
+
+
+// Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b) {
+    a = 30;
+    b.city = 'San Francisco';
+}
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+*/
+
+
+
+/////////////////////////////
+// Lecture: Passing functions as arguments
+
+var years = [1990, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+        return Math.round(206.9 - (0.67 * el));
+    } else {
+        return -1;
+    }
+}
+
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(rates);
