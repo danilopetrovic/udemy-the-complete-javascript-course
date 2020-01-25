f8a58a9f4e401c933c727351dd483dfa1df66d08
Danilo Petrovic <danilo86petrovic@gmail.com>
2020-01-25 01:49:59 +0100

    071 Coding Challenge 7 Solution Part 1

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -33,7 +33,6 @@ console.log(mark.lastName);
 */
 
 
-
 /////////////////////////////
 // Lecture: Object.create
 /*
@@ -54,7 +53,6 @@ var jane = Object.create(personProto, {
     job: { value: 'designer' }
 });
 */
-
 
 
 /////////////////////////////
@@ -98,7 +96,6 @@ console.log(obj.city);
 */
 
 
-
 /////////////////////////////
 // Lecture: Passing functions as arguments
 /*
@@ -138,7 +135,6 @@ console.log(rates);
 */
 
 
-
 /////////////////////////////
 // Lecture: Functions returning functions
 /*
@@ -172,7 +168,6 @@ interviewQuestion('teacher')('Mark');
 */
 
 
-
 /////////////////////////////
 // Lecture: IIFE
 /*
@@ -196,7 +191,6 @@ game();
     console.log(score >= 5 - goodLuck);
 })(5);
 */
-
 
 
 /////////////////////////////
@@ -237,10 +231,9 @@ interviewQuestion('teacher')('John');
 */
 
 
-
 /////////////////////////////
 // Lecture: Bind, call and apply
-
+/*
 var john = {
     name: 'John',
     age: 26,
@@ -274,6 +267,7 @@ johnFriendly('night');
 var emilyFormal = john.presentation.bind(emily, 'formal');
 emilyFormal('afternoon');
 
+
 // Another cool example
 var years = [1990, 1965, 1937, 2005, 1998];
 
@@ -297,3 +291,68 @@ var ages = arrayCalc(years, calculateAge);
 var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 console.log(ages);
 console.log(fullJapan);
+*/
+
+
+/////////////////////////////
+// CODING CHALLENGE
+
+
+/*
+--- Let's build a fun quiz game in the console! ---
+
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+
+2. Create a couple of questions using the constructor
+
+3. Store them all inside an array
+
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+(function () {
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    }
+
+    Question.prototype.displayQuestion = function () {
+        console.log(this.question);
+
+        for (let i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' + this.answers[i]);
+        }
+    };
+
+    Question.prototype.checkAnswer = function (ans) {
+        if (ans === this.correct) {
+            console.log('Correct answer!');
+        } else {
+            console.log('Wrong answer. Try again :)')
+        }
+
+    };
+
+    var q1 = new Question('Is JavaScript the coolest programming language in the world?', ['Yes', 'No'], 0);
+    var q2 = new Question('What\'s the name of this course\'s teacher?', ['John', 'Michael', 'Jonas'], 2);
+    var q3 = new Question('What does best describe coding?', ['Borring', 'Hard', 'Fun', 'Tedious'], 2);
+
+    var questions = [q1, q2, q3];
+
+    var n = Math.floor(Math.random() * questions.length);
+    questions[n].displayQuestion();
+
+    var answer = parseInt(prompt('Please select the correct answer'));
+
+    questions[n].checkAnswer(answer);
+})();
