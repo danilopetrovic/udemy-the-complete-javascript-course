c36bb5f25399abbfe9be3b1835a8e760a796b2e0
Danilo Petrovic <danilo86petrovic@gmail.com>
2022-05-15 21:35:27 +0200

    neka glupost od mog pokusaja

--- 5-advanced-JS/moje resenje/script.js
+++ 5-advanced-JS/moje resenje/script.js
@@ -3,17 +3,54 @@ function correctIncorrectAnswer(color) {
         document.body.style.backgroundColor = color;
         setTimeout(function () {
             document.body.style.backgroundColor = '#999';
-        }, 500);
-    }, 1000);
+        }, 200);
+    }, 300);
 
     setTimeout(function () {
         clearInterval(correctAnswer)
-    }, 4000);
+    }, 600);
 }
 
 function correctAnswer() {
     correctIncorrectAnswer('green');
 }
+
 function incorrectAnswer() {
     correctIncorrectAnswer('red');
 }
+
+function randomNumber() {
+    return Math.ceil(Math.random() * 10);
+}
+
+var a = randomNumber();
+var b = randomNumber();
+var pitanje = document.getElementById('pitanje');
+var odgovor = document.getElementById('odgovor');
+var score = 0;
+var scoreCounter = document.getElementById('scoreCounter');
+
+function question(a, b) {
+    pitanje.innerText = a + ' + ' + b + ' = ?';
+}
+
+question(a, b);
+
+odgovor.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        // console.log('stisnut enter', parseInt(odgovor.value));
+        if (a + b === parseInt(odgovor.value)) {
+            // console.log('tacno');
+            score++;
+            scoreCounter.innerText = score;
+            odgovor.value = '';
+            correctAnswer();
+        } else {
+            // console.log('nije tacno');
+            score--;
+            scoreCounter.innerText = score;
+            odgovor.value = '';
+            incorrectAnswer();
+        }
+    }
+});
